f0b339e24a9ca3d583120a00d3f8f88fc9f04fe7
YanPavlukovich <filin022@gmail.com>
2023-05-02 14:20:18 +0300

    нарисовал default List с помощью MUI, подключил к App

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,9 +1,11 @@
+import { ListArticlesPage } from "../../pages/list-articles-page/ListArticlesPage";
 import { Header } from "../header/Header";
 
 export const App = () => {
   return (
     <div>
       <Header />
+      <ListArticlesPage />
     </div>
   );
 };
--- src/pages/list-articles-page/ListArticlesPage.tsx
+++ src/pages/list-articles-page/ListArticlesPage.tsx
@@ -1,1 +1,17 @@
-export {};
+import { List, ListItem, ListItemText } from "@material-ui/core";
+
+export const ListArticlesPage = () => {
+  return (
+    <List>
+      <ListItem>
+        <ListItemText primary="Article 1" />
+      </ListItem>
+      <ListItem>
+        <ListItemText primary="Article 2" />
+      </ListItem>
+      <ListItem>
+        <ListItemText primary="Article 3" />
+      </ListItem>
+    </List>
+  );
+};
