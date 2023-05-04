8736acf049a32dfc3d68de304a5dcb35f82f3086
YanPavlukovich <filin022@gmail.com>
2023-05-04 16:17:47 +0300

    сделал типизацию статей по взаимодействию с API

---
+++ src/API/articles/articles-types.ts
@@ -0,0 +1,16 @@
+import { PostResponseType } from "../../types/post";
+
+export type GetArticlesType = (limit: number, offset: number, token?: string) => Promise<PostResponseType>;
+
+export type GetArticleType = (slug: string, token?: string) => Promise<PostResponseType>;
+
+export type CreateArticleDataType = {
+	title: string;
+	description: string;
+	body: string;
+	tagList: string[];
+}
+
+export type CreateArticleType = (data: CreateArticleDataType,token:string) => Promise<PostResponseType>;
+export type DeleteArticleType = (slug:string, token:string) => Promise<any>;
+export type UpdateArticleType = (slug:string, token:string, article: CreateArticleDataType) => Promise<PostResponseType>;
