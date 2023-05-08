c576175f71e07a5ebce25c00ab061dd631fb246e
YanPavlukovich <filin022@gmail.com>
2023-05-08 19:25:17 +0300

    хэдер карточки также декомпозируем в отдельный компонент, подключая post-header-info и post-header-profile

--- src/components/post-card/post-header/PostHeader.tsx
+++ src/components/post-card/post-header/PostHeader.tsx
@@ -1,1 +1,31 @@
-export {};
+import { usePostHeaderStyles } from "./usePostHeaderStyles";
+import { PostHeaderInfo, PostHeaderInfoProps } from "./PostHeaderInfo";
+import { PostHeaderProfile, PostHeaderProfileProps } from "./PostHeaderProfile";
+
+type Props = PostHeaderInfoProps & PostHeaderProfileProps;
+
+export const PostHeader = (props: Props) => {
+  const postHeaderStyle = usePostHeaderStyles();
+  const {
+    titleContent,
+    favoritesCount,
+    favorited,
+    tagList,
+    createdAt,
+    author,
+    slug,
+  } = props;
+
+  return (
+    <div className={postHeaderStyle.postHeader}>
+      <PostHeaderInfo
+        titleContent={titleContent}
+        favoritesCount={favoritesCount}
+        favorited={favorited}
+        tagList={tagList}
+        slug={slug}
+      />
+      <PostHeaderProfile createdAt={createdAt} author={author} />
+    </div>
+  );
+};
