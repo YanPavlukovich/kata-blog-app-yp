c8fe258b8d9286e3c5c70e917ede884e2fc210da
YanPavlukovich <filin022@gmail.com>
2023-05-09 00:19:18 +0300

    переписал заглушку под статичный хэдер, теперь тут использованы  AppBar, ToolBar, Button из MUI, также подключил домашнюю кнопку лого, компонент профиля при условии авторизации

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,15 +1,61 @@
-import { AppBar, Toolbar, Typography } from "@material-ui/core";
+import { AppBar, Toolbar } from "@material-ui/core";
+import { Button } from "@mui/material";
+import { Link } from "react-router-dom";
 import { useHeaderStyles } from "./useHeaderStyles";
+import { useAppDispatch, useAppSelector } from "../../store/store-hooks";
+import {
+  removeUserData,
+  selectIsAuth,
+  selectUserData,
+} from "../../store/slices/user-slice";
+import { HomeButton } from "../home-button/HomeButton";
+import { Profile } from "../profile/Profile";
 
 export const Header = () => {
   const headerStyles = useHeaderStyles();
+  const isAuth = useAppSelector(selectIsAuth);
+  const { username, avatar } = useAppSelector(selectUserData);
+  const dispatch = useAppDispatch();
+
+  const onLogout = () => {
+    dispatch(removeUserData);
+  };
   return (
-    <div className={headerStyles.header}>
+    <>
       <AppBar position="static" color="transparent">
-        <Toolbar>
-          <Typography variant="h6">Blog Platform</Typography>
+        <Toolbar className={headerStyles.header}>
+          <HomeButton />
+          {isAuth ? (
+            <div className={headerStyles.authBtns}>
+              <Button
+                component={Link}
+                to={"/new-article"}
+                variant="contained"
+                color="success"
+                size="small"
+              >
+                Create article
+              </Button>
+              <Profile author={{ username, image: avatar }} to={"/profile"} />
+              <Button onClick={onLogout}>Log out</Button>
+            </div>
+          ) : (
+            <div className={headerStyles.btns}>
+              <Button variant="text" component={Link} to={"/sign-in"}>
+                Sign in
+              </Button>
+              <Button
+                variant="outlined"
+                component={Link}
+                to={"/sign-up"}
+                color="success"
+              >
+                Sign up
+              </Button>
+            </div>
+          )}
         </Toolbar>
       </AppBar>
-    </div>
+    </>
   );
 };
--- src/components/header/useHeaderStyles.ts
+++ src/components/header/useHeaderStyles.ts
@@ -2,6 +2,8 @@ import { makeStyles } from "@material-ui/core/styles";
 
 type HeaderStylesType = {
 	header: React.CSSProperties;
+	btns: React.CSSProperties;
+	authBtns: React.CSSProperties;
 }
 
 export const useHeaderStyles = makeStyles<HeaderStylesType>(() => ({
@@ -11,5 +13,17 @@ export const useHeaderStyles = makeStyles<HeaderStylesType>(() => ({
 		display: 'flex',
 		justifyContent: 'space-between',
 		alignItems: 'center',
+	},
+
+	btns: {
+		display: 'flex',
+		alignItems: 'center',
+		gap: '20px',
+	},
+
+	authBtns: {
+		display: 'flex',
+		alignItems: 'center',
+		gap: '25px',
 	}
 }));
