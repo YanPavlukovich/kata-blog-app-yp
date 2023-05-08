ad7b5e8037c2601000471af183f7d07071876d8b
YanPavlukovich <filin022@gmail.com>
2023-05-08 19:39:42 +0300

    убираем старые заглушки для дерева и подключаем первую навигацию пока через routes and route

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,27 +1,16 @@
-import { ArticlesPage } from "../../pages/article-page/ArticlesPage";
-import { CreateArticlePage } from "../../pages/create-article-page/CreateArticlePage";
-import { EditArticlePage } from "../../pages/edit-article-page/EditArticlePage";
-import { EditProfilePage } from "../../pages/edit-profile-page/EditProfilePage";
-import { ListArticlesPage } from "../../pages/list-articles-page/ListArticlesPage";
-import { SignInPage } from "../../pages/sign-in-page/SignInPage";
-import { SignUpPage } from "../../pages/sign-up-page/SignUpPage";
-import { DeleteArticleModal } from "../delete-article-modal/DeleteArticleModal";
-import { Header } from "../header/Header";
+import { Routes, Route } from "react-router-dom";
 import { useAppStyles } from "./useAppStyles";
+import { ListArticlesPage } from "../../pages/list-articles-page/ListArticlesPage";
 
 export const App = () => {
   const appStyles = useAppStyles();
+
   return (
     <div className={appStyles.body}>
-      <Header />
-      <ListArticlesPage />
-      <ArticlesPage />
-      <SignUpPage />
-      <SignInPage />
-      <EditProfilePage />
-      <CreateArticlePage />
-      <EditArticlePage />
-      <DeleteArticleModal />
+      <Routes>
+        <Route index element={<ListArticlesPage />} />
+        <Route path={"/articles"} element={<ListArticlesPage />} />
+      </Routes>
     </div>
   );
 };
