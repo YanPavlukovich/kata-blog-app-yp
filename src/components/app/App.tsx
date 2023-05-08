c790cec12ec6ade4ae25016e6c3cd2e3daee68b3
YanPavlukovich <filin022@gmail.com>
2023-05-08 21:25:48 +0300

    подключил layout к app, обёртка работает, начинаю рисовать роутинг

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import { useAppStyles } from "./useAppStyles";
 import { ListArticlesPage } from "../../pages/list-articles-page/ListArticlesPage";
+import { Layout } from "../layout/Layout";
 
 export const App = () => {
   const appStyles = useAppStyles();
@@ -8,8 +9,16 @@ export const App = () => {
   return (
     <div className={appStyles.body}>
       <Routes>
-        <Route index element={<ListArticlesPage />} />
-        <Route path={"/articles"} element={<ListArticlesPage />} />
+        <Route path={"/"} element={<Layout />}>
+          <Route index element={<ListArticlesPage />} />
+          <Route path={"/articles"} element={<ListArticlesPage />} />
+          <Route
+            path={"articles/:id"}
+            element={
+              "Здесь будет открываться карточка выбранного поста по нажатию на хэдер карточки в списке на главной странице"
+            }
+          />
+        </Route>
       </Routes>
     </div>
   );
