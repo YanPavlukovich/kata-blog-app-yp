c42587facf89d94e0178a1bcab8f91c4cb004c8c
YanPavlukovich <filin022@gmail.com>
2023-05-08 21:19:51 +0300

    создаём Layout для оболочки всего приложения с постоянным статичным хэдером и используем Outlet для рендеринга дочерних маршрутов внутри родительского маршрута

---
+++ src/components/layout/Layout.tsx
@@ -0,0 +1,15 @@
+import { Header } from "../header/Header";
+import { Outlet } from "react-router-dom";
+import { useLayoutStyles } from "./useLayoutStyles";
+
+export const Layout = () => {
+  const layoutStyle = useLayoutStyles();
+  return (
+    <>
+      <Header />
+      <div className={layoutStyle.layout}>
+        <Outlet />
+      </div>
+    </>
+  );
+};
---
+++ src/components/layout/useLayoutStyles.ts
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+type LayoutStylesType = {
+	layout: React.CSSProperties;
+}
+
+export const useLayoutStyles = makeStyles<LayoutStylesType>(() => ({
+	layout: {
+		width: '938px',
+		margin: '0 auto',
+		paddingTop: '26px',
+	},
+}));
