f5f426eca0596474e3d46b69a1c450848ae222cb
YanPavlukovich <filin022@gmail.com>
2023-05-08 19:22:55 +0300

    в хэдере какрточки декомпозируем информацию о посте, подключая типографию article-title, лайки like и теги

--- src/components/post-card/post-header/PostHeaderInfo.tsx
+++ src/components/post-card/post-header/PostHeaderInfo.tsx
@@ -1,5 +1,34 @@
-import React from "react";
+import { ArticleTitle } from "../../../typography/article-title";
+import { Like, LikeProps } from "../../like/Like";
+import { usePostHeaderStyles } from "./usePostHeaderStyles";
 
-export const PostHeaderInfo = () => {
-  return <div>PostHeaderInfo</div>;
+import { Tag } from "../../tag/Tag";
+
+export type PostHeaderInfoProps = LikeProps & {
+  titleContent: string;
+  tagList: string[];
+  slug?: string;
+};
+
+export const PostHeaderInfo = (props: PostHeaderInfoProps) => {
+  const titleStyle = usePostHeaderStyles();
+  const tagStyle = usePostHeaderStyles();
+  const { titleContent, favoritesCount, favorited, tagList, slug } = props;
+  const tags = tagList.map((tag, i) => <Tag key={`tag_${i}`} tag={tag} />);
+
+  return (
+    <>
+      <div className={titleStyle.titleWrapper}>
+        <ArticleTitle level={3} to={slug ? `/articles/${slug}` : null}>
+          {titleContent}
+        </ArticleTitle>
+        <Like
+          favorited={favorited}
+          favoritesCount={favoritesCount}
+          slug={slug}
+        />
+      </div>
+      <div className={tagStyle.tagList}>{tags}</div>
+    </>
+  );
 };
