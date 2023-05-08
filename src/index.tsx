0ab01c0c331755299f61807ccd06944342c642c1
YanPavlukovich <filin022@gmail.com>
2023-05-08 19:42:06 +0300

    убираем старую базовую заглушку для индекса и подключаем store, persistor из store в persist-gate и оборачиваем это все в провайдера, делаем тестовый запуск приложения, рендер списка работает корректно

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import ReactDOM from "react-dom/client";
-
+import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router-dom";
-
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./store/store";
 import { App } from "./components/app/App";
 
 const root = ReactDOM.createRoot(
@@ -9,7 +10,13 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
+  </>
 );
