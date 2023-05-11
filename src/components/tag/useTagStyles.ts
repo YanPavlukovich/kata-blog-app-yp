f424031efb41a81b63fab3d5855a11ea49753a4a
YanPavlukovich <filin022@gmail.com>
2023-05-11 12:54:19 +0300

    style-commit

--- src/components/tag/useTagStyles.ts
+++ src/components/tag/useTagStyles.ts
@@ -1,17 +1,17 @@
 import { makeStyles } from '@material-ui/core/styles';
 
 type TagStylesType = {
-	tag: React.CSSProperties;
+	tagItem: React.CSSProperties;
 }
 
 export const useTagStyles = makeStyles<TagStylesType>(() => ({
-	tag: {
+	tagItem: {
 		display: 'inline-block',
 		fonstStyle: 'normal',
 		fontWeight: 'normal',
 		fontSize: '12px',
 		lineHeight: '15px',
-		color: 'rgba(0, 0, 0, 0.5)',
+		color: 'red',
 		padding: '3px 5px',
 		border: '1px solid rgba(0, 0, 0, 0.5)',
 		borderRadius: '2px',
