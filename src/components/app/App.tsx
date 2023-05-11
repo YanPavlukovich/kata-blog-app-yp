0461dd3859a48b2377efd0a5cd1ea30feda4a80d
YanPavlukovich <filin022@gmail.com>
2023-05-11 12:55:04 +0300

    настроил роутинг имеющихся страниц подлкючил их к App, все корректно работает

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,25 +1,30 @@
 import { Routes, Route } from "react-router-dom";
-import { useAppStyles } from "./useAppStyles";
 import { ListArticlesPage } from "../../pages/list-articles-page/ListArticlesPage";
 import { Layout } from "../layout/Layout";
+import { ArticlePage } from "../../pages/article-page/ArticlePage";
+import { SignUpPage } from "../../pages/sign-up-page/SignUpPage";
+import { ProtectRoute } from "../protect-route/ProtectRote";
+import { useAppSelector } from "../../store/store-hooks";
+import { selectIsAuth } from "../../store/slices/user-slice";
 
 export const App = () => {
-  const appStyles = useAppStyles();
+  const isAuth = useAppSelector(selectIsAuth);
 
   return (
-    <div className={appStyles.body}>
-      <Routes>
-        <Route path={"/"} element={<Layout />}>
-          <Route index element={<ListArticlesPage />} />
-          <Route path={"/articles"} element={<ListArticlesPage />} />
-          <Route
-            path={"articles/:id"}
-            element={
-              "Здесь будет открываться карточка выбранного поста по нажатию на хэдер карточки в списке на главной странице"
-            }
-          />
-        </Route>
-      </Routes>
-    </div>
+    <Routes>
+      <Route path={"/"} element={<Layout />}>
+        <Route index element={<ListArticlesPage />} />
+        <Route path={"/articles"} element={<ListArticlesPage />} />
+        <Route path={"articles/:id"} element={<ArticlePage />} />
+        <Route
+          path={"sign-up"}
+          element={
+            <ProtectRoute isAllowed={!isAuth} redirectPath={"/"}>
+              <SignUpPage />
+            </ProtectRoute>
+          }
+        />
+      </Route>
+    </Routes>
   );
 };
