f5b9a0cb5b40978866c8cae5b2a90f3630bcfd36
YanPavlukovich <filin022@gmail.com>
2023-05-13 20:51:38 +0300

    style-commit подключаем стили для модалки на подтверждение удаления

--- src/pages/edit-article-page/useEditArticleStyles.ts
+++ src/pages/edit-article-page/useEditArticleStyles.ts
@@ -1,3 +1,16 @@
-export {
+import { createStyles, makeStyles } from '@material-ui/core/styles';
 
-};
+type EditArticlePageStylesType = {
+	successMessage: React.CSSProperties;
+}
+
+export const useEditArticlePageStyles = makeStyles<EditArticlePageStylesType>(() =>
+	createStyles({
+		successMessage: {
+			width: '300px',
+			position: 'fixed',
+			right: '30px',
+			bottom: '30px',
+		},
+	})
+);
